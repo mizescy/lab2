075a36b366f14c029fd6aa5c1ca22335fa1a0512
Gordon <gordonboo.23@ichat.sp.edu.sg>
2024-10-29 09:54:46 +0800

    updated bmi.py

--- Bmi.py
+++ Bmi.py
@@ -8,9 +8,12 @@ def calculate_bmi(height, weight):
 
     if bmi < 18.5:
         print("Underweight")
+        return -1
     elif bmi > 18.5 or bmi <= 25:
         print("Normal weight")
+        return 0
     else:
         print("Overweight")
+        return 1
 
 calculate_bmi(1.73,57)
